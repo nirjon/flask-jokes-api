e0a67d1c14a1ad341f903bc5b049da85410ea476
Shahriar Nirjon <nirjon@cs.unc.edu>
2022-08-01 09:41:10 -0400

    Changed the jokes API url which was down.

--- myapp.py
+++ myapp.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
         
 @app.route("/", methods=["GET"])
 def message():
-    resp = requests.get("https://v2.jokeapi.dev/joke/Programming?type=single")
+    resp = requests.get("https://geek-jokes.sameerkumar.website/api")
+    #resp = requests.get("https://v2.jokeapi.dev/joke/Programming?type=single")
     x = json.loads(resp.text)
-    return x["joke"]
+    return x
 
 if __name__=='__main__':
     app.run(debug=True)
