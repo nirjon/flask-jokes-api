78348decef0338cedcb9e8be139ace6236a1451d
Shahriar Nirjon <nirjon@cs.unc.edu>
2022-08-03 13:56:27 -0400

    Code compacted and jokes API is back.

--- myapp.py
+++ myapp.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
         
 @app.route("/", methods=["GET"])
 def message():
-    resp = requests.get("https://geek-jokes.sameerkumar.website/api")
-    #resp = requests.get("https://v2.jokeapi.dev/joke/Programming?type=single")
-    x = json.loads(resp.text)
-    return x
+    #resp = requests.get("https://geek-jokes.sameerkumar.website/api")
+    resp = requests.get("https://v2.jokeapi.dev/joke/Programming?type=single").json()
+    return resp["joke"]
 
 if __name__=='__main__':
     app.run(debug=True)
